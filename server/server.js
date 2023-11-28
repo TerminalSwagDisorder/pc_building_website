c4924db21e03ce3f2484bdc94c58dcb112750164
TerminalSwagDisorder <44695050+TerminalSwagDisorder@users.noreply.github.com>
2023-11-28 11:02:13 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -341,16 +341,18 @@ api.patch("/api/computerwizard", authenticateJWT, async (req, res) => {
 	const userId = req.user.ID; // User ID from the authenticated JWT
 	const formFields = req.body // New data to append
 	
-	if (formFields.price === NaN || formFields.price === 0 || formFields.price === undefined) {
-		return res.status(400).json({ message: "Price must be a number between 500-5000" })
-	} else if (formFields.price < 500 || formFields.price > 5000) {
-		return res.status(400).json({ message: "Price must be between 500-5000" })
-	}
-	if (formFields.otherColor !== "" && formFields.colorPreference !== "other") {
-		formFields.otherColor = ""
-	}
 
 	try {
+		// Some checks for formData data
+		if (formFields.price === NaN || formFields.price === 0 || formFields.price === undefined) {
+			return res.status(400).json({ message: "Price must be a number between 500-5000" })
+		} else if (formFields.price < 500 || formFields.price > 5000) {
+			return res.status(400).json({ message: "Price must be between 500-5000" })
+		}
+		if (formFields.otherColor !== "" && formFields.colorPreference !== "other") {
+			formFields.otherColor = ""
+		}
+
 		// First, retrieve the existing data
 		const selectQuery = "SELECT Completed_builds FROM user WHERE ID = ?";
 		userDb.get(selectQuery, [userId], (err, row) => {
